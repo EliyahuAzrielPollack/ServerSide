27df1ecf82242bd6f3b05e8b83111f98b5d9f3ef
EliyahuAzrielPollack <eli020187@gmail.com>
2015-04-24 14:30:53 +0300

    מסך הגדרות שלב ב

---
+++ ServerSide/src/info/androidhive/slidingmenu/SettingFragment.java
@@ -0,0 +1,17 @@
+package info.androidhive.slidingmenu;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+public class SettingFragment  extends PreferenceFragment  {
+	
+	
+	 @Override
+	 public void onCreate(Bundle savedInstanceState) {
+	  // TODO Auto-generated method stub
+	  super.onCreate(savedInstanceState);
+	  
+	  // Load the preferences from an XML resource
+	        addPreferencesFromResource(R.xml.preferences);
+	 }
+
+}
